b1c111ce146c9dee33972bd20230397e4d4802c9
RichieYe <juest2004@163.com>
2016-07-08 16:55:18 +0800

    继续完成MainActivity类的实现

---
+++ app/src/main/java/com/richieye/examinationsystem/StartTestingActivity.java
@@ -0,0 +1,13 @@
+package com.richieye.examinationsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartTestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_testing);
+    }
+}
